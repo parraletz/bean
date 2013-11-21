45e484f061ccf0fb9f59b94fca05524c02fc2ccd
Alex Parra <parraletz@gmail.com>
2013-11-21 17:39:02 -0600

    Add suit_redactor y configuracion del admin.

--- bean/settings.py
+++ bean/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'suit',
+    'suit_redactor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,25 @@
+from django.forms import ModelForm
 from django.contrib import admin
-
+from suit_redactor.widgets import RedactorWidget
 from .models import Categorias, Posts
 
+
+class PostForm(ModelForm):
+    class Meta:
+        widgets = {
+            'contenido': RedactorWidget(editor_options={'lang': 'es'})
+        }
+
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'slug', 'categoria',)
+    list_display = ('titulo', 'slug', 'categoria','fecha',)
+    form = PostForm
+    fieldsets = [
+        (' ', {'fields': ('titulo',)}),
+        (' ', {'fields': ('contenido',)}),
+        (' ', {'fields': ('categoria',)}),
+
+    ]
 
 
 admin.site.register(Categorias)
